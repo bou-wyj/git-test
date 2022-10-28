e5043c6f115084576c807af8f05d53fcdb04109b
bou-wyj <wyj_bou@163.com>
2022-10-28 20:44:18 +0800

    first IDEA commit

---
+++ src/main/java/com/wyj/bou/GitTest.java
@@ -0,0 +1,13 @@
+package com.wyj.bou;
+
+/**
+ * @program: git-test
+ * @description: Git初识
+ * @author: Mr.wyj
+ * @create: 2022-10-28 20:16
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git~");
+    }
+}
