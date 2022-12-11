ea9fa2956f9ac0cf199b5dca4e4c3642de9a22b2
bou-wyj <wyj_bou@163.com>
2022-12-11 11:45:44 +0800

    b1分支提交

---
+++ src/main/java/com/wyj/bou/MergeB1.java
@@ -0,0 +1,13 @@
+package com.wyj.bou;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: Mr.wyj
+ * @create: 2022-12-11 11:44
+ **/
+public class MergeB1 {
+    public static void main(String[] args) {
+        System.out.println("b1分支的合并。。。。");
+    }
+}
